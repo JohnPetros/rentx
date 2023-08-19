a786de323e9ba36fe1b5e4fbd4ee804ffa7a631f
João Pedro Carvalho <joaopedro.nc@outlook.com>
2023-08-19 18:50:22 +0000

    🔒 Add ensureAuthentication middleware

---
+++ src/middlewares/ensureAthentication.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction } from 'express'
+import { verify } from 'jsonwebtoken'
+
+import { AppError } from '@errors/AppError'
+import { UsersRepository } from '../modules/accounts/repositories/implementations/UsersRepository'
+
+export async function ensureAuthentication(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const authHeader = request.headers.authorization
+
+  if (!authHeader) {
+    throw new AppError('Token missing', 401)
+  }
+
+  const [, token] = authHeader.split(' ')
+
+  try {
+    const { sub: userId } = verify(token, '3f388a638143f3b6aa1e36a4bf8cd17c')
+
+    const usersRepository = new UsersRepository()
+    const user = usersRepository.findById(String(userId))
+
+    if (!user) {
+      throw new AppError('User does not exist', 401)
+    }
+
+    next()
+  } catch (error) {
+    console.error(error)
+    throw new AppError('Ivalid token', 401)
+  }
+}
