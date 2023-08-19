9841556b29c0fb0e707c012206f88f13266d356e
João Pedro Carvalho <joaopedro.nc@outlook.com>
2023-08-19 15:45:00 +0000

    🗄️ Add users database model

---
+++ src/modules/accounts/entities/User.ts
@@ -0,0 +1,33 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm'
+import { v4 as uuid } from 'uuid'
+
+@Entity('users')
+export class User {
+  @PrimaryColumn('uuid')
+  id?: string
+
+  @Column('text')
+  name: string
+
+  @Column('text')
+  username: string
+
+  @Column('text')
+  email: string
+
+  @Column('text')
+  password: string
+
+  @Column('text')
+  driver_license: string
+
+  @CreateDateColumn()
+  created_at: Date
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid()
+    }
+  }
+}
+
