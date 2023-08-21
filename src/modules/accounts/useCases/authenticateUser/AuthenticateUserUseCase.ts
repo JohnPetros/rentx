c341c42fb4ccf38e5391ab84af2c9fc706940802
João Pedro Carvalho <joaopedro.nc@outlook.com>
2023-08-21 13:22:41 +0000

    🔒 Add AppError to AuthenticateUserUseCase

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -4,6 +4,7 @@ import { IUsersRepository } from '../../repositories/IUsersRepository'
 
 import { compare } from 'bcrypt'
 import { sign } from 'jsonwebtoken'
+import { AppError } from '@errors/AppError'
 
 interface IRequest {
   email: string
@@ -29,13 +30,13 @@ export class AuthenticateUserUseCase {
     const user = await this.usersRepository.findByEmail(email)
 
     if (!user) {
-      throw new Error('Email or password incorrect')
+      throw new AppError('Email or password incorrect')
     }
 
     const passwordMatch = await compare(password, user.password)
 
     if (!passwordMatch) {
-      throw new Error('Email or password incorrect')
+      throw new AppError('Email or password incorrect')
     }
 
     const token = sign({}, '3f388a638143f3b6aa1e36a4bf8cd17c', {
