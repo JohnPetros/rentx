8b939afe6e3b665da822fefd65bd18438ff9bc69
João Pedro Carvalho <joaopedro.nc@outlook.com>
2023-08-21 13:21:53 +0000

    🔧 chore: Set up App Error handler

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from 'express'
+import "express-async-errors"
+import express, { NextFunction, Request, Response } from 'express'
 import swaggerUi from 'swagger-ui-express'
 import swaggerConfig from './swagger.json'
 
@@ -7,13 +8,29 @@ import './database'
 import './shared/container'
 
 import { router } from './routes'
+import { AppError } from '@errors/AppError'
 
 const app = express()
 
 app.use(express.json())
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerConfig))
+
 app.use(router)
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerConfig))
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        message: error.message,
+      })
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: `Internal server error - ${error.message}`,
+    })
+  }
+)
 
 app.listen(3333, () => console.log('Server is Running!'))
