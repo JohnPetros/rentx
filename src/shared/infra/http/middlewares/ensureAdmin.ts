2177c58b0c16b9c64fe7ad5de502f1cc5a29a6c8
João Pedro Carvalho <joaopedro.nc@outlook.com>
2023-08-31 14:13:48 +0000

    🔒 Add admin middleware

---
+++ src/shared/infra/http/middlewares/ensureAdmin.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from 'express'
+import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository'
+import { AppError } from '@shared/errors/AppError'
+
+export async function ensureAdmin(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const { id } = request.user
+
+  const usersRepository = new UsersRepository()
+  const user = await usersRepository.findById(id)
+
+  console.log(user);
+  
+
+  if (Boolean(!user.is_admin)) {
+    throw new AppError('User is not a admin!')
+  }
+
+  return next()
+}
