68541745363f675998aa8a9035f4043f5a71efd1
João Pedro Carvalho <joaopedro.nc@outlook.com>
2023-09-01 17:36:03 +0000

    🗄️ Add CreateCarImage migration

--- src/shared/infra/typeorm/migrations/1693576645337-CreateSpecificationCar.ts
+++ src/shared/infra/typeorm/migrations/1693576645337-CreateSpecificationCar.ts
@@ -5,7 +5,7 @@ import {
   TableForeignKey,
 } from 'typeorm'
 
-export class CreateSpecificationCar implements MigrationInterface {
+export class Migrations1693576645337 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
